078e867e979294d5e379cfe3bbd317347524024b
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-03-27 18:29:27 +0000

    Update sidebar-menu.blade.php

--- resources/views/partials/sidebar-menu.blade.php
+++ resources/views/partials/sidebar-menu.blade.php
@@ -3,14 +3,14 @@
     @forelse($pools as $pool)
         <li class="side-nav-item">
             <a href="{{ route('pool-tests.create', $pool->pool_id) }}" class="side-nav-link">
-                <span class="menu-icon"><i class="ti ti-bar-chart"></i></span>
+                <span class="menu-icon"><i class="ti ti-dashboard"></i></span>
                 <span class="menu-text"> {{ $pool->pool_name }} </span>
                 <span class="badge bg-success rounded-pill">5</span>
             </a>
         </li>
     @empty
         <li class="side-nav-item">
-        <span class="menu-icon"><i class="ti ti-bar-chart"></i></span>
+        <span class="menu-icon"><i class="ti ti-dashboard"></i></span>
             <span class="menu-text">No Pools Found</span>
         </li>
     @endforelse
