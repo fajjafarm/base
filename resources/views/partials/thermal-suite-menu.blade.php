95fe4232a7b55d2f7a1ab1ab831ffc579b62b962
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-03-27 18:58:24 +0000

    Update thermal-suite-menu.blade.php

--- resources/views/partials/thermal-suite-menu.blade.php
+++ resources/views/partials/thermal-suite-menu.blade.php
@@ -2,7 +2,7 @@
     <li class="side-nav-title">Thermal Suite Checks</li>
     @forelse($thermalSuites as $suite)
         <li class="side-nav-item">
-            <a href="{{ route('thermal-suite.check', $suite->id) }}" class="side-nav-link">
+            <a href="{{ route('thermal-suites.check', $suite->id) }}" class="side-nav-link">
                 <span class="menu-icon"><i class="ti ti-thermometer"></i></span>
                 <span class="menu-text"> {{ $suite->thermal_name }} </span>
                 @if($suite->needsCheck())
