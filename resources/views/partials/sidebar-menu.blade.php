66938a7d1079690b09cc0e153a0145f33fd9051d
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-03-27 18:19:00 +0000

    Update sidebar-menu.blade.php

--- resources/views/partials/sidebar-menu.blade.php
+++ resources/views/partials/sidebar-menu.blade.php
@@ -2,7 +2,7 @@
     <li class="side-nav-title">Pool Tests</li>
     @forelse($pools as $pool)
         <li class="side-nav-item">
-            <a href="{{ route('pool-tests.create', $pool->id) }}" class="side-nav-link">
+            <a href="{{ route('pool-tests.create', $pool->pool_id) }}" class="side-nav-link">
                 <span class="menu-icon"><i class="ti ti-bar-chart"></i></span>
                 <span class="menu-text"> {{ $pool->pool_name }} </span>
                 <span class="badge bg-success rounded-pill">5</span>
