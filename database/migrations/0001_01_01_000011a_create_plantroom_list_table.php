137f40115e37e742d4c05bb667bb681f77d72b7b
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-03-28 20:04:37 +0000

    Create 0001_01_01_000011a_create_plantroom_list_table.php

---
+++ database/migrations/0001_01_01_000011a_create_plantroom_list_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('plantroom_components', function (Blueprint $table) {
+            $table->id();
+            $table->ulid('plantroom_id');
+            $table->enum('component_type', ['filter', 'strainer', 'cl_injector', 'ph_injector', 'pac_injector']);
+            $table->integer('component_number');
+            $table->text('description')->nullable();
+            $table->timestamps();
+
+            $table->foreign('plantroom_id')->references('plantroom_id')->on('plantroom_list')->onDelete('cascade');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('plantroom_components');
+    }
+};
