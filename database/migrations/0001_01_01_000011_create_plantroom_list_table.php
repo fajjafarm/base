bbc7f79571a881a50c517e91ba2006b43507e06d
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-03-28 19:59:46 +0000

    Update 0001_01_01_000011_create_plantroom_list_table.php

--- database/migrations/0001_01_01_000011_create_plantroom_list_table.php
+++ database/migrations/0001_01_01_000011_create_plantroom_list_table.php
@@ -5,28 +5,21 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Str;
 
-class CreatePlantroomListTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    public function up(): void
     {
         Schema::create('plantroom_list', function (Blueprint $table) {
-            $table->ulid('plantroomid')->primary();
+            $table->ulid('plantroom_id')->primary();
             $table->string('client_id');
             $table->string('plantroom_name');
-            $table->decimal('filters');
-            $table->decimal('strainers');
-            $table->decimal('cl_injector');
-            $table->decimal('ph_injector');
-            $table->decimal('pac_injector');
-            $table->string('info');
+            $table->text('description')->nullable();
             $table->timestamps();
-
-            
         });
     }
 
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('plantroom_list');
     }
-}
+};
