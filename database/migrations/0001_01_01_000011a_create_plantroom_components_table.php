3fabaa355bc9d46b66ffa4ddeb84c5ddfd698dba
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-03-29 18:47:17 +0000

    1

--- database/migrations/0001_01_01_000011a_create_plantroom_components_table.php
+++ database/migrations/0001_01_01_000011a_create_plantroom_components_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('plantroom_components', function (Blueprint $table) {
             $table->id();
             $table->ulid('plantroom_id');
-            $table->enum('component_type', ['filter', 'strainer', 'cl_injector', 'ph_injector', 'pac_injector']);
+            $table->enum('component_type', ['filter', 'strainer', 'cl_injector', 'ph_injector', 'pac_injector', 'pump']);
             $table->integer('component_number');
             $table->text('description')->nullable();
             $table->timestamps();
