e9f4f80294131bcfa4ed6d262b7c4ff23ed1b2e3
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-03-29 20:05:46 +0000

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\SuperAdminOnly;
+use App\Http\Middleware\SuperAdminOnly; // Ensure this is present
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,9 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        'superadmin' => SuperAdminOnly::class, // Register superadmin middleware
-        //
+        $middleware->alias([
+            'superadmin' => SuperAdminOnly::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
