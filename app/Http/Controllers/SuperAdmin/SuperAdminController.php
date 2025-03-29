02b0df2c98d046aa9ad3d32e7454ec3e2035bfd9
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-03-29 19:53:04 +0000

    1

--- app/Http/Controllers/SuperAdmin/SuperAdminController.php
+++ app/Http/Controllers/SuperAdmin/SuperAdminController.php
@@ -8,7 +8,7 @@ class SuperAdminController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
-        $this->middleware('superadmin');
+        $this->middleware('auth');      // Ensure user is logged in
+        $this->middleware('superadmin'); // Restrict to super admins
     }
 }
--- app/Http/Kernal.php
+++ app/Http/Kernal.php
@@ -1,5 +1,18 @@
 <?php
-protected $routeMiddleware = [
-    // Existing middleware...
-    'superadmin' => \App\Http\Middleware\SuperAdminOnly::class,
-];
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    // ...
+
+    protected $routeMiddleware = [
+        // Existing middleware...
+        'auth' => \App\Http\Middleware\Authenticate::class,
+        'superadmin' => \App\Http\Middleware\SuperAdminOnly::class, // Add this line
+    ];
+
+    // ...
+}
