2de72559609aec6b51fc18fbf22e950ba857d5e6
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-04-13 18:50:39 +0100

    1

--- app/Models/WaterMeter.php
+++ app/Models/WaterMeter.php
@@ -3,33 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Str;
 
 class WaterMeter extends Model
 {
     protected $table = 'water_meters';
 
-    protected $primaryKey = 'water_meter_id';
-    public $incrementing = false;
-    protected $keyType = 'string';
-
     protected $fillable = [
-        'water_meter_id', 'plantroom_id', 'location', 'description'
+        'plantroom_id',
+        'location',
+        'description',
     ];
-
-    protected static function boot()
-    {
-        parent::boot();
-
-        static::creating(function ($model) {
-            if (empty($model->water_meter_id)) {
-                $model->water_meter_id = Str::ulid();
-            }
-        });
-    }
 
     public function plantroom()
     {
         return $this->belongsTo(PlantroomList::class, 'plantroom_id', 'plantroom_id');
+    }
+
+    public function readings()
+    {
+        return $this->hasMany(WaterMeterReading::class, 'water_meter_id', 'id');
     }
 }
