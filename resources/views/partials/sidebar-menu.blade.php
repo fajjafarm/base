47fd11c28806e24c717671aa9a1b2c2ce2764afc
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-04-13 19:37:44 +0100

    1

--- resources/views/partials/sidebar-menu.blade.php
+++ resources/views/partials/sidebar-menu.blade.php
@@ -14,26 +14,4 @@
             <span class="menu-text">No Pools Found</span>
         </li>
     @endforelse
-    <li class="side-nav-item">
-        <a data-bs-toggle="collapse" href="#waterMetersCollapse" aria-expanded="false" aria-controls="waterMetersCollapse" class="side-nav-link">
-            <span class="menu-icon"><i class="ti ti-gauge"></i></span>
-            <span class="menu-text">Water Meters</span>
-            <span class="menu-arrow"><i class="ti ti-chevron-down"></i></span>
-        </a>
-        <div class="collapse" id="waterMetersCollapse">
-            <ul class="side-nav-second-level">
-                @forelse($waterMeters as $meter)
-                    <li class="side-nav-item">
-                        <a href="{{ route('water-meter.readings.index', $meter->water_meter_id) }}" class="side-nav-link">
-                            <span class="menu-text">{{ $meter->location }}</span>
-                        </a>
-                    </li>
-                @empty
-                    <li class="side-nav-item">
-                        <span class="menu-text">No Water Meters Found</span>
-                    </li>
-                @endforelse
-            </ul>
-        </div>
-    </li>
-</ul>
+    
