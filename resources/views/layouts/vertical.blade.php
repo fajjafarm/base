4f09c56559dd58a7b9ac5acb4062d357327de805
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-04-14 19:06:43 +0100

    Update vertical.blade.php

--- resources/views/layouts/vertical.blade.php
+++ resources/views/layouts/vertical.blade.php
@@ -29,9 +29,7 @@
     @include('layouts.partials.customizer')
 
     @include('layouts.partials.footer-scripts')
-</body>
 
-</html>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
@@ -50,3 +48,6 @@
     </script>
 @endpush
 @stack('js')
+</body>
+
+</html>
